feed8b627b4d0855247ec254c55acdd48ba7e8a7
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-10 23:57:28 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+#general idea for setup
+setup(
+    name="Moodipy",
+    version="0.1",
+    packages=["Moodipy", "SpotifyAuth", "SentimentAnalysis", "PlaylistGenerator", "SongAnalysis", "GUI"],
+    url="https://github.com/dianas11xx/Moodify",
+    author="Noteworthy",
+    description="use sentiment analysis to create a playlist that matches someone's mood and also predict songs to rise in popularity.",
+    install_requires=["python3.8", "Ubuntu", "WSL2"],
+    entry_points={"console_scripts": ["start_Moodipy=Moodipy:main"]}
+)
