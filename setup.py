11df3d6eac184ed116ca59be30321222d54aa34a
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-14 00:12:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     url="https://github.com/dianas11xx/Moodify/",
     author="Noteworthy",
     description="use sentiment analysis to create a playlist that matches someone's mood and also predict songs to rise in popularity.",
-    install_requires=["python3.8", "Ubuntu", "WSL2"],
+    install_requires=["python3.8", "Ubuntu", "PyQt5"],
     entry_points={"console_scripts": ["start_Moodipy=Moodipy:main"]}
 )
