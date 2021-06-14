27d9ec4e16dfa9913b28a9518de3e2a3ca829306
Lazaro Couso <lcouso@ufl.edu>
2021-06-13 20:11:22 -0400

    I read it

--- Moodipy/Spotipy.py
+++ Moodipy/Spotipy.py
@@ -21,7 +21,7 @@ class Spotify(object):
         self._client_id = client_id
         self._client_secret = client_secret
         self._redirect_uri = redirect_uri
-        self.spotify_client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id, client_secret))
+        self._spotify_client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id, client_secret))
 
         if not self._spotify_client:
             raise Exception("An error occurred on the client side")
@@ -36,7 +36,7 @@ class User(Spotify):
     # Constructor
     def __init__(self, user_id=None, scope=None, client=None):
         self._user_id = user_id
-        self.user_token = util.prompt_for_user_token(user_id, scope,client._client_id, client._client_secret, client._redirect_uri)
+        self._user_token = util.prompt_for_user_token(user_id, scope,client._client_id, client._client_secret, client._redirect_uri)
         self._user_client = spotipy.Spotify(auth=self._user_token)
 
         if not self._user_client:
@@ -46,32 +46,32 @@ class User(Spotify):
     def get_playlist_id(self, playlist_name=None):
         if playlist_name == None:
             raise Exception("You must enter a playlist name")
-        
+
         playlist_id = None
         playlists = self._user_client.user_playlists(self.user_id)
         for playlist in playlists['items']:
             if playlist['name'] == playlist_name:
                 playlist_id = playlist['id']
-        
+
         if playlist_id == None:
             raise Exception("There is no playlist with that name")
-        
+
         return playlist_id
 
     # Creates a New Playlist for the User
     def create_playlist(self, playlist_name=None, public=True, collaborative=False, description=""):
         if playlist_name == None:
             raise Exception("You must enter a playlist name")
-        
+
         self._user_client.user_playlist_create(self._user_id, name=playlist_name, public=public, collaborative=collaborative, description=description)
-    
+
     # Adds Tracks to a User's Playlist
     def add_to_playlist(self, playlist_name=None, playlist_tracks=None, amount=100):
         if playlist_name == None:
             raise Exception("You must enter a playlist name")
         if amount > 10000 or amount < 1:
             raise Exception("You must enter a valid amount")
-        
+
         playlist_id = self.get_playlist_id(playlist_name=playlist_name)
         track_ids = (track['id'] for track in playlist_tracks[:amount])
 
