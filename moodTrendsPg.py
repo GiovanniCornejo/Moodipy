4d97300d2c319f4a26593f84ac73c37906c1a31b
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-22 18:00:36 -0400

    add moodTrendsPg.py
    This class presents the user's current mood that we analyzed or submitted. right now it only prints out the user's mood.

---
+++ moodTrendsPg.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from UserSummary import Person
+
+class MoodTrends(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = "Mood Trends"
+        self.left = 9
+        self.top = 100
+        self.width = 1000
+        self.height = 610
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.setStyleSheet("background-color: #bdffcc")
+        self.mood_window()
+        self.show()
+
+    def mood_window(self):
+        Person.setLabel(self, "Mood Analysis", False, 20, 10, 370, 39, 20, "#bdffcc", True, 'Segoe UI')
+        Person.setLabel(self, "Your Current Mood", False, 25, 100, 370, 39, 12, "#bdffcc", True, 'Segoe UI')
+        Person.setLabel(self, "Your Mood Trends", False, 470, 330, 370, 39, 12, "#bdffcc", True, 'Segoe UI')
+        Person.setLabel(self, "", False, 20, 58, 225, 3, 0, "black", False, 'Segoe UI')
+        Person.setLabel(self, "", False, 25, 150, 480, 170, 0, "white", False, 'Segoe UI')
+        Person.setLabel(self, "", False, 470, 370, 480, 170, 0, "white", False, 'Segoe UI')
+        Person.setLabel(self, Person.currentmood, False, 90, 280, 120, 20, 11, "white", False, 'Consolas')
+
+
+
