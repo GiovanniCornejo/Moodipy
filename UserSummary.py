40f33e3259d76bda0f160846129c5381b8b3cd76
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-22 17:57:29 -0400

    add UserSummary
    This holds all the user's information, such as their current mood, username, and password. Also has the setLabel function that is used by many other classes

---
+++ UserSummary.py
@@ -0,0 +1,63 @@
+import datetime
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+class Person:
+    def __init__(self):
+        self.__username = None
+        self.__password = None
+        self.__currentMood = None
+        date = datetime.datetime.now().date()
+        self.__moods = {}
+        self.__moods[date] = self.__currentMood
+        self.__userID = None
+
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, username):
+        self.__username = username
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, password):
+        self.__password = password
+
+    @property
+    def currentmood(self):
+        return self.__currentMood
+
+    @property
+    def moods(self):
+        return self.__moods
+
+    @currentmood.setter
+    def currentmood(self, currentMood):
+        self.__currentMood = currentMood
+
+    @property
+    def userID(self):
+        return self.__userID
+
+    @userID.setter
+    def userID(self, userID):
+        self.__userID = userID
+
+    def setLabel(self, text, center, left, top, width, height, ftSize, bkColor, bold, font):
+        label = QLabel(text, self)
+        if center:
+            label.setAlignment(Qt.AlignCenter)
+        label.setGeometry(left, top, width, height)
+        style = "background-color: "+bkColor+";"
+        if bold:
+            style = style+"font-weight: bold"
+        label.setStyleSheet(style)
+        label.setFont(QFont(font, ftSize))
+
