6cf6f0841570446f7a73cfe7daaac5dee053a0b2
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-22 17:54:27 -0400

    Update LoadPage.py
    Updated so it actually puts the user's mood in and reads the setLabel function correctly

--- LoadPage.py
+++ LoadPage.py
@@ -4,6 +4,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from UserSummary import Person
 
 
 class LoadPg(QMainWindow):
@@ -25,23 +26,12 @@ class LoadPg(QMainWindow):
 
     def mood_window(self):
         # Labels
-        self.setLabel("Generating your", True, 390, 200, 220, 35, 19, "white", False)
-        self.setLabel("Down", True, 380, 280, 230, 58, 35, "white", True)
-        self.setLabel("Playlist", True, 390, 380, 220, 35, 19, "white", False)
+        Person.setLabel(self, "Generating your", True, 390, 200, 220, 35, 19, "white", False, 'Segoe UI')
+        Person.setLabel(self, Person.currentmood, True, 380, 280, 230, 58, 35, "white", True, 'Segoe UI')
+        Person.setLabel(self, "Playlist", True, 390, 380, 220, 35, 19, "white", False, 'Segoe UI')
 
     def paintEvent(self, event):
         paint = QPainter(self)
         paint.setPen(QPen(Qt.white, 5, Qt.SolidLine))
         paint.setBrush(QBrush(Qt.white, Qt.SolidPattern))
         paint.drawEllipse(270, 70, 450, 450)
-
-    def setLabel(self, text, center, left, top, width, height, ftSize, bkColor, bold):
-        label = QLabel(text, self)
-        if center:
-            label.setAlignment(Qt.AlignCenter)
-        label.setGeometry(left, top, width, height)
-        style = "background-color: "+bkColor+";"
-        if bold:
-            style = style+"font-weight: bold"
-        label.setStyleSheet(style)
-        label.setFont(QFont('Segoe UI', ftSize))
