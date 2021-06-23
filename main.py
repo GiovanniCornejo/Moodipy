9e3d50aa7792303beddf6350c501dffc57f4ebc0
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-23 17:33:18 -0400

    Scale adjustments
    Scale the labels and size of widgets based off the desktop size

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from UserSummary import Person
 from MoodAnalyzerGUI import MoodAnalyzerPg
-from UserLogin import UserLoginPG
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.title = "Moodipy"
-        self.left = 9
-        self.top = 100
-        self.width = 1000
-        self.height = 610
+        self.desktop = QApplication.desktop()
+        self.left = 0
+        self.top = 0
+        self.width = self.desktop.width()-100
+        self.height = self.desktop.height()-100
         self.initUI()
 
 
@@ -26,25 +26,28 @@ class MainWindow(QMainWindow):
         self.show()
 
     def main_window(self):
+        print(self.width)
+        print(self.height)
         button = QPushButton(self)
-        button.setGeometry(480, 450, 40, 20)
+        button.setGeometry(self.width/2.08, self.height/1.36, self.width/25, self.height/30.5)
         button.setStyleSheet("border-image : url(imgs/arrow.png);")
         button.clicked.connect(self.next_page)
-        Person.setLabel(self, "Welcome to", True, 390, 200, 220, 35, 19, "white", False,'Consolas')
-        Person.setLabel(self, "Moodipy", True, 380, 260, 230, 58, 35, "white", True, 'Segoe UI')
-        Person.setLabel(self, "Generating playlists that express how you feel", True, 335, 375, 338, 20, 9, "white", False,'Consolas')
+        Person.setLabel(self, "Welcome to", True, self.width / 2.56, self.height / 3.05, self.width / 4.55, self.height / 17.43, (self.width/1280)*19, "white", False, 'Consolas')
+        Person.setLabel(self, "Moodipy", True, self.width/2.63, self.height/2.35, self.width/4.35, self.height/10.5, (self.width/1280)*35, "white", True, 'Segoe UI')
+        Person.setLabel(self, "Generating playlists that express how you feel", True, self.width/2.99, self.height/1.63, self.width/2.99, self.height/30.5, (self.width/1280)*9, "white", False, 'Consolas')
 
 
     def paintEvent(self, event):
         paint = QPainter(self)
         paint.setPen(QPen(Qt.white, 5, Qt.SolidLine))
         paint.setBrush(QBrush(Qt.white, Qt.SolidPattern))
-        paint.drawEllipse(270, 70, 450, 450)
+        paint.drawEllipse(self.width/3.6875, self.height/10.3, self.width/2.22, self.width/2.22)
 
     def next_page(self):
         self.nextPg = MoodAnalyzerPg()
         self.nextPg.show()
         self.hide()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
