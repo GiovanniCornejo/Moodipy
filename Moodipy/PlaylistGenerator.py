2e8d5a9098e163729fa37eb9b26275110efe9253
Giselle <58486290+Gisellegr92@users.noreply.github.com>
2021-06-25 19:18:32 -0400

    Update PlaylistGenerator.py

--- Moodipy/PlaylistGenerator.py
+++ Moodipy/PlaylistGenerator.py
@@ -5,11 +5,10 @@ import SpotifyAuthorization
 from SpotifyAuthorization import client
 from SpotifyAuthorization import user
 
-
 """ 
 Step 0: Get Users Liked Songs
 """
-user_songs = user.get_user_saved_tracks() 
+user_songs = user.get_user_saved_tracks()
 # Check that all songs are gotten
 # for i, track in enumerate(user_songs, 1):
 #     print(i, track['name'])
@@ -20,7 +19,33 @@ Step 1: Get Tracks Matching User Emotion
 """
 first_emotion = "sadness"
 second_emotion = "awful"
-emotion_tracks = user.get_user_emotion_tracks(client=client,user_tracks=user_songs, base_emotion=first_emotion, second_emotion=second_emotion)
+emotion_tracks = user.get_user_emotion_tracks(client=client, user_tracks=user_songs, base_emotion=first_emotion,
+                                              second_emotion=second_emotion)
+
+first_emotion = "bad"
+second_emotion = "anger"
+emotion_tracks = user.get_user_emotion_tracks(client=client, user_tracks=user_songs, base_emotion=first_emotion,
+                                              second_emotion=second_emotion)
+
+first_emotion = "okay"
+second_emotion = "fear"
+emotion_tracks = user.get_user_emotion_tracks(client=client, user_tracks=user_songs, base_emotion=first_emotion,
+                                              second_emotion=second_emotion)
+
+first_emotion = "happy"
+second_emotion = "joy"
+emotion_tracks = user.get_user_emotion_tracks(client=client, user_tracks=user_songs, base_emotion=first_emotion,
+                                              second_emotion=second_emotion)
+
+first_emotion = "excited"
+second_emotion = "surprise"
+emotion_tracks = user.get_user_emotion_tracks(client=client, user_tracks=user_songs, base_emotion=first_emotion,
+                                              second_emotion=second_emotion)
+
+first_emotion = "love"
+second_emotion = None
+emotion_tracks = user.get_user_emotion_tracks(client=client, user_tracks=user_songs, base_emotion=first_emotion,
+                                              second_emotion=second_emotion)
 # Check that all songs are gotten
 # for i, track in enumerate(f_emotion_tracks, 1):
 #     print(i, track['name'])
@@ -36,3 +61,4 @@ user.create_playlist(playlist_name=playlist_name, description=description)
 Step 3: Add Songs to a Playlist
 """
 user.add_to_playlist(playlist_name=playlist_name, playlist_tracks=emotion_tracks)
+
