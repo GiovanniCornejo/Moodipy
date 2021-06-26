324ba7df18d2d679ec4b36421a4fefb20df3f4d8
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-25 21:02:08 -0400

    Updated UserSummary.py
    Creates playlist names and organizes mood labels based off the amount of moods detected

--- UserSummary.py
+++ UserSummary.py
@@ -1,34 +1,52 @@
-import datetime
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 class Person:
     def __init__(self):
-        self.__username = None
-        self.__password = None
         self.__currentMood = []
-        date = datetime.datetime.now().date()
-        self.__moods = {}
-        self.__moods[date] = self.__currentMood
         self.__userID = None
-
-
-    @property
-    def username(self):
-        return self.__username
-
-    @username.setter
-    def username(self, username):
-        self.__username = username
+        self.__moodLabel = None
+        self.__playlistName = None
 
     @property
-    def password(self):
-        return self.__password
+    def playlistName(self):
+        return self.__playlistName
 
-    @password.setter
-    def password(self, password):
-        self.__password = password
+    @playlistName.setter
+    def playlistName(self, title):
+       self.__playlistName = title
+
+    def setPlaylistName(self, label, currMood):
+        playlistName = ''
+        if label == "Happy" or currMood[0] == "joy":
+            playlistName = "Happy Vibes"
+        elif label == "Awful" or currMood[0] == "sadness":
+            playlistName = "Downbeat and Angst"
+        elif label == "Bad" or currMood[0] == "anger":
+            playlistName = "Rage and Loathing"
+        elif label == "Okay" or currMood[0] == "fear":
+            playlistName = "It's Gonna be Okayyy"
+        elif label == "Excited" or currMood[0] == "surprise":
+            playlistName = "Hyped Up!"
+        elif "Love" in label:
+            playlistName = "In Love with Life"
+        return playlistName
+
+    @property
+    def moodLabel(self):
+        return self.__moodLabel
+
+    @moodLabel.setter
+    def moodLabel(self, label):
+        self.__moodLabel = label
+
+    def setMoodLabel(self, currMood):
+        if len(currMood) == 1:
+            moodLabel = currMood[0].capitalize()
+        else:
+            moodLabel = currMood[0].capitalize() + " and " + currMood[1].capitalize()
+        return moodLabel
 
     @property
     def currentmood(self):
@@ -60,4 +78,3 @@ class Person:
             style = style+"font-weight: bold"
         label.setStyleSheet(style)
         label.setFont(QFont(font, ftSize))
-
