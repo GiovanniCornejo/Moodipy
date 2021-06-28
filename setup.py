540dc744ef05f26a312ab3c8f917e427ae7d4e74
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-28 05:15:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     python_requires = '==3.8',
     package_data = {"Moodipy":['imgs/awful.jpeg', 'imgs/arrow.png', 'emotions.txt']},
     description="use sentiment analysis to create a playlist that matches someone's mood and also predict songs to rise in popularity.",
-    install_requires=["spotipy>=2.18", "nltk>=3.6.2", "pyqt5>=5.12","screeninfo>=0.6.5", "requests"],
+    install_requires=['nltk==3.6.2', 'screeninfo==0.6.7', 'spotipy==2.18.0', 'requests==2.22.0', 'PyQt5==5.15.4'],
     entry_points={"console_scripts": ["start_Moodipy=Moodipy.main:main"]},
     include_package_data=True
 )
