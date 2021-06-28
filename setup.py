68c4ae54dc7bb1fc0ee80408b754043d82d7900c
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-28 05:04:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
-#general idea for setup
+
 setup(
     name="Moodipy",
-    version="0.1",
-    packages=["Moodipy", "GUI"],
-    url="https://github.com/dianas11xx/Moodify/",
+    version="0.9.1",
+    packages=['Moodipy',],
+    url="https://github.com/dianas11xx/Moodify",
     author="Noteworthy",
+    license = "GPL 3",
+    python_requires = '==3.8',
+    package_data = {"Moodipy":['imgs/awful.jpeg', 'imgs/arrow.png', 'emotions.txt']},
     description="use sentiment analysis to create a playlist that matches someone's mood and also predict songs to rise in popularity.",
-    install_requires=["python3.8", "Ubuntu", "PyQt5"],
-    entry_points={"console_scripts": ["start_Moodipy=Moodipy:main"]}
+    install_requires=["spotipy>=2.18", "nltk>=3.6.2", "pyqt5>=5.12","screeninfo>=0.6.5", "requests"],
+    entry_points={"console_scripts": ["start_Moodipy=Moodipy.main:main"]},
+    include_package_data=True
 )
