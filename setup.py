40c94fdc999425de958371dad893493583619fc7
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-28 23:37:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name="Moodipy",
-    version="0.9.1",
-    packages=['Moodipy',],
-    url="https://github.com/dianas11xx/Moodify",
+    version="0.112",
+    packages=['Moodipy'],
+    url="https://github.com/dianas11xx/MoodipyTest",
     author="Noteworthy",
     license = "GPL 3",
-    python_requires = '==3.8',
-    package_data = {"Moodipy":['imgs/awful.jpeg', 'imgs/arrow.png', 'emotions.txt']},
-    description="use sentiment analysis to create a playlist that matches someone's mood and also predict songs to rise in popularity.",
-    install_requires=['nltk==3.6.2', 'screeninfo==0.6.7', 'spotipy==2.18.0', 'requests==2.22.0', 'PyQt5==5.15.4'],
-    entry_points={"console_scripts": ["start_Moodipy=Moodipy.main:main"]},
+    long_description = "Uses sentiment analysis to create a playlist that matches someone's mood and also predict songs to rise in popularity.",
+    description="Spotify Playlist Generator based of Mood",
+    install_requires=['nltk==3.6.2', 'screeninfo==0.6.7', 'spotipy==2.18.0', 'requests>=2.25.0', 'PyQt5>=5.12','urllib3==1.26.6', 'PyQt5-sip>=4.19.19'],
+    entry_points={"console_scripts": ["Moodipy=Moodipy.main:main"]},
     include_package_data=True
 )
+
