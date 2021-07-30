f68ad8f2a0e036512dfa6d4c3f555e12cd8e5dc0
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-07-30 02:58:43 -0400

    Updated install_requires
    MoodAnalyzer no longer uses Nltk library. In order to take shorter sentances and increase accuracy, it uses pandas>=1.3.0, scikit-learn>=0.24.2, and joblib>=1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     package_data = {"Moodipy":["*.txt", "imgs/*.png", "imgs/*.jpeg"]},
     long_description = "Uses sentiment analysis to create a playlist that matches someone's mood and also predict songs to rise in popularity.",
     description="Spotify Playlist Generator based of Mood",
-    install_requires=['nltk==3.6.2', 'screeninfo==0.6.7', 'spotipy==2.18.0', 'requests>=2.25.0', 'PyQt5>=5.12','urllib3==1.26.6', 'PyQt5-sip>=4.19.19'],
+    install_requires=['screeninfo==0.6.7', 'spotipy==2.18.0', 'requests>=2.25.0', 'PyQt5>=5.12','urllib3==1.26.6', 'PyQt5-sip>=4.19.19', 'pandas>=1.3.0', 'scikit-learn>=0.24.2', 'joblib==1.0.1'],
     entry_points={"console_scripts": ["Moodipy=Moodipy.main:main"]},
 )
-
